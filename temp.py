46fac63f984fa306012a8924d541c29db7c93123
Vivek-c29 <vivekchauhan4773@gmail.com>
2025-01-30 23:36:00 +0530

    Gramamr correcter NLP Model

---
+++ temp.py
@@ -0,0 +1,14 @@
+import matplotlib
+import nltk
+import numpy
+import pandas
+import seaborn
+import torch
+import transformers
+
+print(matplotlib.__version__,
+nltk.__version__,
+numpy.__version__,
+pandas.__version__,
+torch.__version__,
+transformers.__version__)
