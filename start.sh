4a66a162b41b62b23e1818c9c15f21d655f60d7c
Vivek-c29 <vivekchauhan4773@gmail.com>
2025-02-01 23:13:58 +0530

    Commit after changes in start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 mkdir -p models
 
 # Download the model from GitHub Releases
-wget -O models/model.safetensors "https://github.com/vivek-c29/Grammar-Correcter/releases/download/v1.0/model.safetensors"
+wget -O Models/t5_grammar_correction/model.safetensors "https://github.com/vivek-c29/Grammar-Correcter/releases/download/v1.0/model.safetensors"
 
 # Start the application
 pip install sentencepiece
