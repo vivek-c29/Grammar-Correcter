bcc01ad3da50cfd515f31530a47e2617de1fa25f
Vivek-c29 <vivekchauhan4773@gmail.com>
2025-02-01 19:27:35 +0530

    Grammar correcter Initial commit

--- Grammar-correcter.git/README.md
+++
@@ -1,2 +0,0 @@
-# Grammar-correcter
-Grammar Correction Model  This project is an NLP-based model designed to automatically correct grammar in sentences. It processes user input and provides grammatically correct output using natural language processing techniques.
--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 mkdir -p models
 
 # Download the model from GitHub Releases
-wget -O models/model.safetensors "https://github.com/vivek-c29/Grammar-correcter/releases/download/v1.0/model.safetensors"
+wget -O models/model.safetensors "https://github.com/vivek-c29/Grammar-Correcter/releases/download/v1.0/model.safetensors"
 
 # Start the application
 python app.py
